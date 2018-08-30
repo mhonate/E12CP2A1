cc00abe919d09a5366d3e19bac2dffbfd5fec866
Marcelo <marcelo.onate@gmail.com>
2018-08-29 22:00:22 -0300

    add: namespace, vistas, container

--- boosass/config/initializers/assets.rb
+++ boosass/config/initializers/assets.rb
@@ -8,6 +8,10 @@ Rails.application.config.assets.version = '1.0'
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
 Rails.application.config.assets.paths << Rails.root.join('vendor', 'stylesheets')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'images')
+
+Rails.application.config.assets.precompile += %w( *.jpg )
+Rails.application.config.assets.precompile += %w( *.png )
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
